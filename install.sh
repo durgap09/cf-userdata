57824784c89f987b85346985a3821c028d5f73e1
durgap09 <90268800+durgap09@users.noreply.github.com>
2022-03-22 16:53:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ sudo apt upgrade -y
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo apt update -y
-sudo apt install ruby-full -y
-sudo apt install wget -y
-wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto > /tmp/logfile
-sudo service codedeploy-agent start
-sudo service codedeploy-agent status
+#sudo apt install ruby-full -y
+#sudo apt install wget -y
+#wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
+#chmod +x ./install
+#sudo ./install auto > /tmp/logfile
+#sudo service codedeploy-agent start
+#sudo service codedeploy-agent status
