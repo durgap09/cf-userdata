75d6f6be4547b8df20bf7743df955756a9de3ff1
durgap09 <90268800+durgap09@users.noreply.github.com>
2022-03-22 08:18:19 +0530

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt install awscli -y
+
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 456325780689.dkr.ecr.us-east-2.amazonaws.com
+
+docker pull 456325780689.dkr.ecr.us-east-2.amazonaws.com/qcloud-backend:latest
+
+docker pull 456325780689.dkr.ecr.us-east-2.amazonaws.com/qcloud-postgres:latest
+
+docker pull 456325780689.dkr.ecr.us-east-2.amazonaws.com/qcloud-pulumi:latest
+
+docker pull 456325780689.dkr.ecr.us-east-2.amazonaws.com/qcloud-frontend:latest
+
+docker volume create qcloud-db
+
+docker volume create qcloud-backend
+
+docker network create appmodz
