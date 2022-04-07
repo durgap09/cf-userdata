a03812bb18e1ef401cd82e3d45922a914054165f
durgap09 <90268800+durgap09@users.noreply.github.com>
2022-04-08 03:30:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,8 @@ wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-
 
 sudo dpkg -i amazon-cloudwatch-agent.deb
 
+sudo mv config.json /opt/aws/amazon-cloudwatch-agent/bin/
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status
