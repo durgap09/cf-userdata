d51d4a12f14c70ba92e611934e3b63a423379401
durgap09 <90268800+durgap09@users.noreply.github.com>
2022-04-11 02:48:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
+#sudo apt update -y
+#sudo apt upgrade -y
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
-sudo apt update -y
+#sudo apt update -y
 #sudo apt install ruby-full -y
 #sudo apt install wget -y
 #wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
